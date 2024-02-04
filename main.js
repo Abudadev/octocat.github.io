c41fcf2ef05fb7ca942afdbced1e27fea1ae35b0
Abudadev <132628423+Abudadev@users.noreply.github.com>
2024-02-03 17:18:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function infopage(){
+    window.location.replace("info.html");
+}
+
+function vocabpage(){
+    window.location.replace("Vocab.html");
+}
+
+function homepage(){
+    window.location.replace("index.html");
+}
